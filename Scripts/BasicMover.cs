95c967d9d09babeb8bd454060c3e122bcb6f4e17
Sumit Kumar Kansagara <35989230+sumit-kansagara1711@users.noreply.github.com>
2019-01-24 00:34:14 +0530

    Add files via upload

---
+++ Scripts/BasicMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMover : MonoBehaviour {
+
+    public float spinSpeed = 180.0f ;
+    public float motion = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+	// Update is called once per frame
+	void Update () {
+    if(doSpin)
+          gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+    if(doMotion)
+          gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * motion);
+            }
+}
